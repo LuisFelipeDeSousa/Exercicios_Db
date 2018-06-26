79ed0827ee5e0c33203f8fda603bdc3dd1130bfd
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-26 17:26:55 -0300

    gfdgf

---
+++ ExercicioAlunos_UPDATE.sql
@@ -0,0 +1,10 @@
+﻿-- Exercicio UPDATE de alunos --
+
+UPDATE alunos SET nota_1 = 9.9 WHERE YEAR (data_nascimento) = 1996
+
+UPDATE alunos SET nota_2 = 1.3 WHERE nome LIKE 'Urbano%'
+
+
+
+
+SELECT * FROM alunos;
