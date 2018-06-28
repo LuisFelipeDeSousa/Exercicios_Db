880ced0a57af6ce13c7953523e5ee941e05f3070
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-28 15:24:23 -0300

    fgdf

---
+++ ExercicioCidades_DELETE.sql
@@ -0,0 +1,24 @@
+﻿-- Apagar a(s) cidade(s) do estado do Rio Grande do Sul.
+
+DELETE FROM cidades WHERE estado = 'RS'
+
+-- Apagar a(s) cidade(s) do estado do Acre que a cidade comece com a letra ‘R’.
+
+DELETE FROM cidades WHERE estado = 'AC' AND cidade LIKE 'R%'  
+
+-- Apagar a(s) cidade(s) que o nome termine com ‘goas’.
+
+DELETE FROM cidades WHERE nome LIKE '%goas'
+
+-- Apagar a(s) cidade(s) que o nome contenha irmãos em qualquer parte .
+
+DELETE FROM cidades WHERE nome LIKE '%irmãos%'
+
+-- Apagar a(s) cidade(s) do estado de Minas Gerais.
+
+DELETE FROM cidades WHERE estado = 'MG'
+
+-- Apagar a(s) cidade(s) que o nome seja ‘Douradina’.
+
+DELETE FROM cidades WHERE nome = 'Douradina'
+
