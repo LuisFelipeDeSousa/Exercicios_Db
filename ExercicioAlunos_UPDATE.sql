12aaeb0c1ddcd0dfedc227281061621c8e5e84be
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-06-28 16:53:35 -0300

    HJHGJ

--- ExercicioAlunos_UPDATE.sql
+++ ExercicioAlunos_UPDATE.sql
@@ -16,4 +16,18 @@ UPDATE alunos SET categoria = 'Ninjutsu' WHERE categoria = 'Fueusn'
 
 UPDATE alunos SET categoria = 'Roxolandia' WHERE cor_preferida = 'Roxo' OR cor_preferida ='Coral'
 
-SELECT * FROM alunos;
+UPDATE alunos SET nota_1 = 1, nota_2 = 1, nota_3 = 1, nota_4 = 1 WHERE ((nota_1+nota_2+nota_3+nota_4)/4) < 4; 
+
+UPDATE alunos SET categoria = 'Dobermann', cor_preferida = 'Rosa' WHERE categoria = 'Saxiol'
+
+UPDATE alunoos SET data_nascimento = CONVERT(CHAR(4), DATEPART(yyyy, data_nascimento)) + '-' + CONVERT(CHAR(2), 
+DATEPART(mm, data_nascimento)) + '30' WHERE DAY(data_nascimento) = 31; 
+
+
+UPDATE alunoos SET data_nascimento = CONVERT(CHAR(4), DATEPART(mm, data_nascimento)) + '-' + CONVERT(CHAR(4), 
+DATEPART(mm, data_nascimento)) + '-' + CONVERT(CHAR(2)
+
+
+ 
+
+
